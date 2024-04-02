3a35e7af1f982c859a64a564038d9bce149bf14f
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-04-02 22:38:49 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Main from './Main';
+import ResponsiveExample from './OffCanvas';
+import BasicExample from './Nav';
+
+
+function App() {
+  return (
+    <>
+    <BasicExample></BasicExample>
+    <Main></Main>
+    <ResponsiveExample></ResponsiveExample>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Main.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+import Badge from 'react-bootstrap/Badge';
+
+function Main() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch
+      </Button>
+
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+
+      <div>
+      <h1>
+        Example heading
+        <Badge bg="secondary" as={Button}>
+          New
+        </Badge>
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default Main
